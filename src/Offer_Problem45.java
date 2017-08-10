d35407c49ad681047566aee787959263b450941a
zhaoshq <523618033@qq.com>
2017-08-10 18:28:53 +0800

    《剑指offer》第45题：圆圈中最后剩下的数字

---
+++ src/Offer_Problem45.java
@@ -0,0 +1,17 @@
+/**
+ * 《剑指offer》圆圈中最后剩下的数字
+ * Created by zhaoshq on 2017/8/10.
+ */
+public class Offer_Problem45 {
+    public int lastRemaining(int n, int m){
+        if (n<1 || m<1){
+            return -1;
+        }
+        int last = 0;
+        for (int i=2; i<m; i++){
+            //这个公式是数学推到出来的
+            last = (last+m)%i;
+        }
+        return last;
+    }
+}
