296f74f75fec59b05d0498e8df8017756112ce9a
zhaoshiqiang <523618033@qq.com>
2017-08-26 14:20:38 +0800

    第14题 返回一组字符串中最长公共前缀

--- src/other/Longest_Common_Prefix.java
+++ src/other/Longest_Common_Prefix.java
@@ -1,7 +1,11 @@
-package dynamic_programming;
+package other;
 
 /**第14
  * Write a function to find the longest common prefix string amongst an array of strings.
+ * 给定一个String类型数组，要求写一个方法，返回数组中这些字符串的最长公共前缀。
+ * 举个例子：假如数组为["123","12","4"]，经过这个方法返回的结果就应该是""。
+ * 因为"123"，"12"，"4"并没有共同的前缀，虽然"123"，"12"的公共最长前缀是"12"，
+ * 但是这个公共前缀"12"与"4"没有公共前缀，所以最后返回的结果就是""。
  * Created by zhaoshiqiang on 2017/2/11.
  */
 //字符串最长公共前缀
