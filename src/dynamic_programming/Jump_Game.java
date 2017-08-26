d17f74a2d0b41e502a8cdee4c5df8a84bbfece63
zhaoshiqiang <523618033@qq.com>
2017-08-26 14:15:31 +0800

    第65题 局部最优解+全局最优解

--- src/dynamic_programming/Jump_Game.java
+++ src/dynamic_programming/Jump_Game.java
@@ -3,14 +3,11 @@ package dynamic_programming;
 /**
  * 第55题
  *  Given an array of non-negative integers, you are initially positioned at the first index of the array.
-
      Each element in the array represents your maximum jump length at that position.
-
      Determine if you are able to reach the last index.
 
      For example:
      A = [2,3,1,1,4], return true.
-
      A = [3,2,1,0,4], return false.
  * Created by zhaoshiqiang on 2017/7/16.
  */
