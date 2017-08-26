9c41d236720d315c86429a3da929ddbe5fc66bc5
zhaoshiqiang <523618033@qq.com>
2017-08-26 11:06:09 +0800

    第70题 动态规划 爬楼梯

--- src/dynamic_programming/Climbing_Stairs.java
+++ src/dynamic_programming/Climbing_Stairs.java
@@ -5,7 +5,7 @@ package dynamic_programming;
  * You are climbing a stair case. It takes n steps to reach to the top.
 
     Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- Note: Given n will be a positive integer.
+    Note: Given n will be a positive integer.
  * Created by zhaoshiqiang on 2017/7/16.
  */
 //线性动态规划
