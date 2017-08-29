f7fbf6c55ef26606483e26f5975eed40dab5f8df
zhaoshiqiang <523618033@qq.com>
2017-08-29 15:14:28 +0800

    第83题 删除有序链表中重复节点

--- src/linklist/Remove_Duplicates_from_Sorted_List.java
+++ src/linklist/Remove_Duplicates_from_Sorted_List.java
@@ -2,9 +2,11 @@ package linklist;
 
 /** 第83题
  * Given a sorted linked list, delete all duplicates such that each element appear only once.
+
      For example,
      Given 1->1->2, return 1->2.
      Given 1->1->2->3->3, return 1->2->3.
+
  * Created by zhaoshiqiang on 2017/2/1.
  */
 public class Remove_Duplicates_from_Sorted_List {
