0a41106dc524bce69222048cfe820a46f2c77249
zhaoshiqiang <523618033@qq.com>
2017-08-29 10:12:37 +0800

    第96题 区间动态规划，生成二叉搜索树的个数

--- src/dynamic_programming/Unique_Binary_Search_Trees.java
+++ src/dynamic_programming/Unique_Binary_Search_Trees.java
@@ -3,8 +3,8 @@ package dynamic_programming;
 /**第96题 生成二叉搜索树
  * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 
- For example,
- Given n = 3, there are a total of 5 unique BST's.
+     For example,
+     Given n = 3, there are a total of 5 unique BST's.
  * Created by zhaoshiqiang on 2017/1/3.
  */
 //区间动态规划
@@ -20,9 +20,9 @@ public class Unique_Binary_Search_Trees {
         results[1]=1;
         results[0]=1;
         /**
-         * 假设 f(i) 表示已升序排序的数组 [0,i] 所存在的不同BST的集合，
-         * 则f(i)可以看出将0~i-1拆分成两段，前段作为i的父节点，后端作为i的左节点，将前段种类*后端种类则为这次分段的总数
-         * 将这些总数求和，则为f(i)
+         * 假设 results(i) 表示已升序排序的数组 [0,i] 所存在的不同BST的集合，
+         * 则results(i)可以看出将0~i-1拆分成两段，前段作为i的父节点，后端作为i的左节点，将前段种类*后端种类则为这次分段的总数
+         * 将这些总数求和，则为results(i)
          * */
         for (int i=2; i<=n; i++){
             for (int j=0; j<i; j++){
