6e47ab4a631af60601c23ec4a1d8be3101246680
zhaoshiqiang <523618033@qq.com>
2017-08-29 14:22:45 +0800

    第127题 算法 BFS，数据结构：哈希表

--- src/graph/Word_Ladder.java
+++ src/graph/Word_Ladder.java
@@ -7,21 +7,20 @@ import java.util.*;
  * find the length of shortest transformation sequence from beginWord to endWord, such that:
      1、Only one letter can be changed at a time.
      2、Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
- For example,
 
- Given:
- beginWord = "hit"
- endWord = "cog"
- wordList = ["hot","dot","dog","lot","log","cog"]
- As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
- return its length 5.
-
- Note:
-     Return 0 if there is no such transformation sequence.
-     All words have the same length.
-     All words contain only lowercase alphabetic characters.
-     You may assume no duplicates in the word list.
-     You may assume beginWord and endWord are non-empty and are not the same.
+    For example,
+     Given:
+     beginWord = "hit"
+     endWord = "cog"
+     wordList = ["hot","dot","dog","lot","log","cog"]
+     As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+     return its length 5.
+     Note:
+         Return 0 if there is no such transformation sequence.
+         All words have the same length.
+         All words contain only lowercase alphabetic characters.
+         You may assume no duplicates in the word list.
+         You may assume beginWord and endWord are non-empty and are not the same.
  * Created by zhaoshiqiang on 2017/1/11.
  */
 //算法 BFS，数据结构：哈希表
