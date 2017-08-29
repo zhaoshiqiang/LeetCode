99373f3e540660f4cbb5bdc36ed18f201f5640e8
zhaoshiqiang <523618033@qq.com>
2017-08-29 14:13:40 +0800

    第77题 DFS 组合数

--- src/graph/Combinations.java
+++ src/graph/Combinations.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 /**第77
  * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
- For example,
- If n = 4 and k = 2, a solution is:
-     [
-         [2,4],
-         [3,4],
-         [2,3],
-         [1,2],
-         [1,3],
-         [1,4],
-     ]
+     For example,
+     If n = 4 and k = 2, a solution is:
+         [
+             [2,4],
+             [3,4],
+             [2,3],
+             [1,2],
+             [1,3],
+             [1,4],
+         ]
  * Created by zhaoshiqiang on 2017/2/16.
  */
-//组合  回溯
+//DFS
 public class Combinations {
 
     public List<List<Integer>> combine(int n, int k) {
