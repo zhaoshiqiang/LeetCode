e76b3c2210560ab76dd9baf88ed9b3b373c8aa6b
zhaoshiqiang <523618033@qq.com>
2017-09-05 10:29:55 +0800

    第32题 栈 括号匹配

--- src/stack/Longest_Valid_Parentheses.java
+++ src/stack/Longest_Valid_Parentheses.java
@@ -3,10 +3,10 @@ package stack;
 import java.util.Stack;
 
 /**32题
- * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ * Given a string containing just the characters '(' and ')',
+ * find the length of the longest valid (well-formed) parentheses substring.
 
      For "(()", the longest valid parentheses substring is "()", which has length = 2.
-
      Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
  * Created by zhaoshiqiang on 2017/1/26.
  */
