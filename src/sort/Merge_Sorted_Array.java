04968482b93f40b0dd1714e4e481b3fa1d9998c8
zhaoshiqiang <523618033@qq.com>
2017-09-05 09:32:05 +0800

    第88题 数组归并排序

--- src/sort/Merge_Sorted_Array.java
+++ src/sort/Merge_Sorted_Array.java
@@ -2,12 +2,14 @@ package sort;
 
 /**第88
  * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
  * Note:
     You may assume that nums1 has enough space (size that is greater or equal to m + n)
     to hold additional elements from nums2. The number of elements initialized in nums1
     and nums2 are m and n respectively.
  * Created by zhaoshiqiang on 2017/2/11.
  */
+//归并排序
 public class Merge_Sorted_Array {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -32,7 +34,4 @@ public class Merge_Sorted_Array {
         }
     }
 
-    public static void main(String[] args){
-
-    }
 }
