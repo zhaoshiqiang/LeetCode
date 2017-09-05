3e51cdbf3833eefbebe8d1c4f995f2dba90baeb6
zhaoshiqiang <523618033@qq.com>
2017-09-05 10:28:09 +0800

    第84题 栈 波峰图

--- src/stack/Largest_Rectangle_in_Histogram.java
+++ src/stack/Largest_Rectangle_in_Histogram.java
@@ -3,12 +3,14 @@ package stack;
 import java.util.Stack;
 
 /**第84题
- * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
- Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
- The largest rectangle is shown in the shaded area, which has area = 10 unit.
- For example,
- Given heights = [2,1,5,6,2,3],
- return 10.
+ * Given n non-negative integers representing the histogram's bar height
+ * where the width of each bar is 1, find the area of largest rectangle in the histogram.
+     Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+     The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+     For example,
+     Given heights = [2,1,5,6,2,3],
+     return 10.
  * Created by zhaoshiqiang on 2017/1/23.
  */
 //栈
