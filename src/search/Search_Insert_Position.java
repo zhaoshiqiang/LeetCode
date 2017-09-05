7e45ca6cc82cc6fc734b95df96e9c6da0da1f1a0
zhaoshiqiang <523618033@qq.com>
2017-09-05 11:11:16 +0800

    第35题 二分查找 在数组中找出<=目标值的最大下标

--- src/search/Search_Insert_Position.java
+++ src/search/Search_Insert_Position.java
@@ -1,7 +1,8 @@
 package search;
 
 /**第35
- * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
 
      You may assume no duplicates in the array.
      Here are few examples.
@@ -25,9 +26,26 @@ public class Search_Insert_Position {
         return length;
     }
 
+    public static int searchInsertII(int[] nums, int target){
+        int mid;
+        int l=0;
+        int h = nums.length-1;
+
+        while (l <= h){
+            mid = (l + h)/2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] < target){
+                l = mid + 1;
+            }else {
+                h = mid-1;
+            }
+        }
+        return l;
+    }
     public static void main(String[] args){
         int[] nums = new int[]{1,3,5,6};
-        System.out.println(searchInsert(nums,5));
+        System.out.println(searchInsertII(nums,2));
 
     }
 
