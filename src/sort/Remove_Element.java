7940734a673633ded8d6fd577dcdfcd9f3958876
zhaoshiqiang <523618033@qq.com>
2017-09-05 09:56:59 +0800

    第27题 快排思想

--- src/sort/Remove_Element.java
+++ src/sort/Remove_Element.java
@@ -2,15 +2,11 @@ package sort;
 
 /**第27
  * Given an array and a value, remove all instances of that value in place and return the new length.
-
- Do not allocate extra space for another array, you must do this in place with constant memory.
-
- The order of elements can be changed. It doesn't matter what you leave beyond the new length.
-
- Example:
- Given input array nums = [3,2,2,3], val = 3
-
- Your function should return length = 2, with the first two elements of nums being 2.
+    Do not allocate extra space for another array, you must do this in place with constant memory.
+    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+     Example:
+     Given input array nums = [3,2,2,3], val = 3
+    Your function should return length = 2, with the first two elements of nums being 2.
  * Created by zhaoshiqiang on 2017/2/22.
  */
 //类快速排序
